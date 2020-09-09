391d85dceaaf5fddf2eb27b15af01fa18e27e2e5
Rowland Sanders <48600494+RowlandSanders@users.noreply.github.com>
2020-09-08 19:05:52 -0700

    Create Card.cpp

---
+++ Solitaire Prime 282 C++ Version/Card.cpp
@@ -0,0 +1,40 @@
+#include "Card.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+    
+
+    Card::Card(){
+        
+    }
+    Card::Card(char r,char s) {
+        suit=s;
+        rank=r;
+    }
+    void Card::setCard(char r,char s){
+        suit=s;
+        rank=r;
+        
+    }
+    int Card:: getValue(){
+        if (rank=='A'){
+            return 1;
+        } else {
+            if (rank>='2' && rank<='9') {
+                return rank-48;
+            } else {
+                if (rank=='J'||rank=='Q'||rank=='K'||rank=='T') {
+                    return 10;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+    void Card:: showCard(){
+        if (rank=='T'){
+            cout<<"10"<<suit;
+        } else {
+          cout<<rank<<suit;
+        }
+    }
